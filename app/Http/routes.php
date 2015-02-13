6c0c54cfe5abf2296d52d424c34fe03f10dce0ba
Terry Harvey <mrterrymh@gmail.com>
2015-02-13 18:38:54 +0000

    Comments in routes.php

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -5,6 +5,7 @@ Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
 
 // Backend
 Route::group(['prefix' => 'backend'], function() {
+	// Authentication-required routes
 	Route::group(['middleware' => 'auth'], function() {
 		// Main dashboard
 		Route::get('/', ['as' => 'backend', 'uses' => 'Backend\HomeController@index']);
@@ -12,7 +13,7 @@ Route::group(['prefix' => 'backend'], function() {
 
 	// Authentication
 	Route::get('/logout', ['as' => 'backend.logout', 'uses' => 'Backend\AuthController@index']);
-	
+
 	Route::get('/login', ['as' => 'backend.login', 'uses' => 'Backend\AuthController@show']);
 	Route::post('/login', 'Backend\AuthController@create');
 });
