d7723f58240410743d2b8d59a4a4862f1403d258
Terry Harvey <mrterrymh@gmail.com>
2015-02-13 18:05:42 +0000

    Create migrations

---
+++ database/migrations/2015_02_13_180058_create_tags_table.php
@@ -0,0 +1,39 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateTagsTable extends Migration
+{
+	/**
+	 * Run the migrations.
+	 *
+	 * @return void
+	 */
+	public function up()
+	{
+		Schema::create('tags', function(Blueprint $table) {
+			$table->increments('id');
+			$table->string('name');
+			$table->string('slug');
+		});
+
+		Schema::create('taggable', function(Blueprint $table) {
+			$table->increments('id');
+			$table->integer('tag_id');
+			$table->integer('taggable_id');
+			$table->integer('taggable_type');
+		});
+	}
+
+	/**
+	 * Reverse the migrations.
+	 *
+	 * @return void
+	 */
+	public function down()
+	{
+		Schema::drop('tags');
+		Schema::drop('taggable');
+	}
+}
