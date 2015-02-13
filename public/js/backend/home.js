f292c62fc0afd2a2f3eda3a87b63b601d6a184f0
Terry Harvey <mrterrymh@gmail.com>
2015-02-13 19:15:18 +0000

    Remove setTimeout on version checker
    That was just for testing.

--- public/js/backend/home.js
+++ public/js/backend/home.js
@@ -1,23 +1,25 @@
 $(document).ready(function() {
-	setTimeout(function() {
-		var updateChecker = $('#update-checker');
-		var updateCheckerBody = updateChecker.find('.panel-body');
-
-		$.get('/version', function(data) {
-			if (data.newer) {
-				var content = '<p>There is a new version of LBlog available (<strong>' + data.latestVersion + '</strong>)</p>';
-				var content = content + '<p><a href="#">Click here for more information.</a></p>';
-
-				updateChecker.removeClass('panel-default').addClass('panel-danger');
-				updateCheckerBody.html(content);
-
-				return;
-			}
-
-			var content = '<p>Your version of LBlog is up to date!</p>';
-
-			updateChecker.removeClass('panel-default').addClass('panel-success');
-			updateCheckerBody.html(content);
-		});
-	}, 1000);
+	checkForUpdates();
 });
+
+function checkForUpdates() {
+	var updateChecker = $('#update-checker');
+	var updateCheckerBody = updateChecker.find('.panel-body');
+
+	$.get('/version', function(data) {
+		if (data.newer) {
+			var content = '<p>There is a new version of LBlog available (<strong>' + data.latestVersion + '</strong>)</p>';
+			var content = content + '<p><a href="#">Click here for more information.</a></p>';
+
+			updateChecker.removeClass('panel-default').addClass('panel-danger');
+			updateCheckerBody.html(content);
+
+			return;
+		}
+
+		var content = '<p>Your version of LBlog is up to date!</p>';
+
+		updateChecker.removeClass('panel-default').addClass('panel-success');
+		updateCheckerBody.html(content);
+	});
+}
