c4f3509f134a0f79187a0ba8f5d070f275da1c2f
Terry Harvey <mrterrymh@gmail.com>
2015-02-13 17:36:14 +0000

    Pull in jQuery, Bootstrap and Font Awesome

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -3,9 +3,12 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>{!! $title !!} | {{ Config::get('site.name') }}</title>
+		<link rel="stylesheet" href="{{ URL::asset('css/bootstrap.min.css') }}">
+		<link rel="stylesheet" href="{{ URL::asset('css/font-awesome.min.css') }}">
 	</head>
-	
+
 	<body>
-		
+		<script src="{{ URL::asset('js/jquery-1.11.2.min.js') }}"></script>
+		<script src="{{ URL::asset('js/bootstrap.min.js') }}"></script>
 	</body>
 </html>
