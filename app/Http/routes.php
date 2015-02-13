19b4caa90394fce81a65d9815d96ff60e0b5ea38
Terry Harvey <mrterrymh@gmail.com>
2015-02-13 18:14:01 +0000

    Create routes for backend

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -1,3 +1,13 @@
 <?php
 
+// Home route
 Route::get('/', ['as' => 'home', 'uses' => 'HomeController@index']);
+
+// Backend
+Route::group(['middleware' => 'auth', 'prefix' => 'backend'], function() {
+	// Main dashboard
+	Route::get('/', ['as' => 'backend', 'uses' => 'Backend\HomeController@index']);
+
+	// Authentication
+	Route::get('/login', ['as' => 'backend.login', 'uses' => 'Backend\AuthController@create']);
+});
