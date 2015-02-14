1d1fbe497c48fddd763cf2ef3a57e1aff912297a
Terry Harvey <mrterrymh@gmail.com>
2015-02-14 21:57:28 +0000

    Add comments to FileController

--- app/Http/Controllers/Backend/FileController.php
+++ app/Http/Controllers/Backend/FileController.php
@@ -24,20 +24,29 @@ class FileController extends BackendController
 		if (!Request::ajax())
 			App::abort(404);
 
+		// Grab the file form the posted data.
 		$file = Input::file('file');
+
+		// Detect the file extension.
 		$extension = File::extension($file->getClientOriginalName());
 
+		// Ensure the file extension is allowed.
 		if (!in_array($extension, $this->allowedExtensions))
 			return Response::make('That file extension is not allowed.', 500);
 
+		// Define the upload directory.
 		$directory = public_path() . '/uploads/';
+
+		// Create an MD5 hash to use as the file name.
 		$filename = md5($file->getClientOriginalName() . time()) . '.' . $extension;
 
+		// Upload the file.
 		$upload = $file->move($directory, $filename);
 
+		// Return an error if the upload failed.
 		if (!$upload)
 			return Response::make('Upload failed. Please try again later.', 500);
-
+		
 		return Response::json([
 			'imagePath' => $directory . $filename
 		]);
